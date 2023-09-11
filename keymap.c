e8ebf1b7e54ab3f5dc1a8b974b8bb10f82b3269a
Hannah Morrison <hannah@landhmorrison.scot>
2023-09-11 11:58:27 +0100

    Add backspace to num layer and history search key

--- keymap.c
+++ keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,         KC_Q,           LCTL_T(KC_W),          LALT_T(KC_F),         LGUI_T(KC_P),           KC_B,    KC_J,    RGUI_T(KC_L),          RALT_T(KC_U),         RCTL_T(KC_Y),           KC_NUBS,        KC_DEL,
     KC_TAB,         KC_A,           LT(CTRL_LAYER, KC_R),  LT(NAV_LAYER, KC_S),  LT(RSYM_LAYER, KC_T),  KC_G,    KC_M,    LT(LSYM_LAYER, KC_N),  LT(NUM_LAYER, KC_E),  LT(FKEYS_LAYER, KC_I),  KC_O,           KC_BSPC,
     KC_CAPS,        KC_Z,           KC_X,                  KC_C,                 KC_D,                  KC_V,    KC_K,    KC_H,                  KC_COMM,              KC_DOT, KC_SLSH,                 OSL(SCUT_LAYER),
-    OSM(MOD_LSFT), OSM(MOD_LCTL),  OSM(MOD_LALT),  OSM(MOD_LGUI),         OSM(MOD_LSFT),         KC_SPC,  KC_ENT,  KC_UP, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+    LCTL(KC_R),     OSM(MOD_LCTL),  OSM(MOD_LALT),  OSM(MOD_LGUI),         OSM(MOD_LSFT),         KC_SPC,  KC_ENT,  KC_UP, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
   ),
 
   [LSYM_LAYER] = LAYOUT_planck_grid(
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [NUM_LAYER] = LAYOUT_planck_grid(
-    KC_TRNS,  KC_PAST,  KC_1,     KC_2,     KC_3,     KC_PPLS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,
+    KC_TRNS,  KC_PAST,  KC_1,     KC_2,     KC_3,     KC_PPLS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_BSPC,
     KC_TRNS,  KC_PSLS,  KC_4,     KC_5,     KC_6,     KC_PMNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,
     KC_TRNS,  KC_0,     KC_7,     KC_8,     KC_9,     KC_DOT,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
